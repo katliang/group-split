57a9b48e9fb96b811e08fb48013e029549488fed
katliang <katliang@outlook.com>
2016-12-22 02:10:06 -0800

    Added edit and update methods and edit view. Formatted code.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,28 +1,43 @@
 class ReportsController < ApplicationController
-    def index
-        @reports = Report.all
-    end
+  def index
+    @reports = Report.all
+  end
 
-    def show
-        @report = Report.find(params[:id])
-    end
+  def show
+    @report = Report.find(params[:id])
+  end
 
-    def new
-        @report = Report.new
-    end
+  def new
+    @report = Report.new
+  end
 
-    def create
-        @report = Report.new(report_params)
+  def edit
+    @report = Report.find(params[:id])
+  end
 
-        if @report.save
-            redirect_to @report
-        else
-            render 'new'
-        end
-    end
+  def create
+    @report = Report.new(report_params)
 
-    private
-    def report_params
-        params.require(:report).permit(:name)
+    if @report.save
+      redirect_to @report
+    else
+      render 'new'
     end
+  end
+
+  def update
+    @report = Report.find(params[:id])
+
+    if @report.update(report_params)
+      redirect_to @report
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:name)
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,10 +1,10 @@
 class Report < ApplicationRecord
-    after_initialize :init
+  after_initialize :init
 
-    def init
-        self.report_uid ||= SecureRandom.uuid
-    end
+  def init
+    self.report_uid ||= SecureRandom.uuid
+  end
 
-    validates :name, presence: true,
-                     length: { minimum: 1 }
+  validates :name, presence: true,
+                   length: { minimum: 1 }
 end
