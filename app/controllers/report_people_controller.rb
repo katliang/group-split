783a796df95585aaa03510ba323a6a8969cc4a3e
katliang <katliang@outlook.com>
2016-12-30 19:01:20 -0600

    A person can now be removed from a report.

--- app/controllers/report_people_controller.rb
+++ app/controllers/report_people_controller.rb
@@ -1,4 +1,9 @@
 class ReportPeopleController < ApplicationController
-  def index
+  def destroy
+    @report = Report.find_by_uuid(params[:report_uuid])
+    @report_person = @report.report_people.find(params[:id])
+
+    @report_person.destroy
+    redirect_to report_path(@report)
   end
 end
