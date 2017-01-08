78297e6286c72e7796e31b8bd40482aa9c016161
katliang <katliang@outlook.com>
2017-01-08 15:31:53 -0800

    Added tests to reports controller and updated test data.

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get reports_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_report_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    report = reports(:one)
+    get report_url(report)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    report = reports(:one)
+    get edit_report_url(report)
+    assert_response :success
+  end
 end
