c9038481a510e91e54488b3e271e52a8a8617ee8
katliang <katliang@outlook.com>
2017-01-08 01:14:08 -0800

    Updated test data and added tests to report model.

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -15,6 +15,21 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test "new report should get uuid" do
     report = Report.new
-    assert_not_nil(report.uuid, "A report uuid should be assigned to a new report.")
+    assert_not_nil report.uuid, "A report uuid should be assigned to a new report."
+  end
+
+  test "sum of report expenses" do
+    report = reports(:one)
+    assert_equal report.sum_expenses, 19.98, "Sum of report expenses is incorrect."
+  end
+
+  test "count of report people" do
+    report = reports(:one)
+    assert_equal report.num_people, 2, "Count of report people is incorrect."
+  end
+
+  test "individual obligation calculation" do
+    report = reports(:one)
+    assert_equal report.individual_obligation, 9.99, "Individual obligation calculation is incorrect."
   end
 end
